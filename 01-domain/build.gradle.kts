040e5e4cbd56ec03d1dba0603c1c43d26f27b591
Alberto Marfoglia <alberto.marfoglia@studio.unibo.it>
2021-11-23 18:44:41 +0100

    Add tuprolog dependencies

--- 01-domain/build.gradle.kts
+++ 01-domain/build.gradle.kts
@@ -3,5 +3,10 @@ plugins {
 }
 
 dependencies {
-   api("it.unibo.tuprolog:core-jvm:0.20.4")
+    api("it.unibo.tuprolog:core-jvm:0.20.4")
+    api("it.unibo.tuprolog:serialize-core-jvm:0.20.4")
+    api("it.unibo.tuprolog:theory-jvm:0.20.4")
+    api("it.unibo.tuprolog:serialize-theory-jvm:0.20.4")
+
+
 }

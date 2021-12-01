0b53c80fe63682f55796752f643e53980b1468ba
Alberto Marfoglia <alberto.marfoglia@studio.unibo.it>
2021-12-01 17:31:24 +0100

    Remove serialization module (useless)

--- 03-delivery-vertx/build.gradle.kts
+++ 03-delivery-vertx/build.gradle.kts
@@ -6,7 +6,6 @@ dependencies {
     implementation(project(":00-utils"))
     implementation(project(":01-domain"))
     implementation(project(":02-core"))
-    implementation(project(":02-serialization"))
     implementation("io.vertx:vertx-core:4.2.1")
     implementation("io.vertx:vertx-lang-kotlin:4.2.1")
     implementation("io.vertx:vertx-web:4.2.1")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,6 @@ rootProject.name = "clean-architecture-example"
 include("00-utils")
 include("01-domain")
 include("02-core")
-include("02-serialization")
 include("03-gateway-inmemory")
 include("03-delivery-vertx")
 include("04-main-vertx-inmemory")

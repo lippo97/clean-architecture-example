23fdbe398628a11cce8e6b7a478477384a3a725d
Alberto Marfoglia <alberto.marfoglia@studio.unibo.it>
2021-12-03 16:07:27 +0100

    Remove useless `this`

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,6 @@ repositories {
 }
 
 dependencies {
-//    implementation("org.jlleitschuh.gradle:ktlint-gradle:10.2.0")
     implementation(kotlin("gradle-plugin", "1.6.0"))
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.19.0")
 }
